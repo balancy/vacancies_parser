1bc6979ae98bca8cbe25e10f74b22be5b3a36931
balancy <balancy@gmail.com>
2021-04-04 10:28:59 +0200

    Move waling through result pages to separate function in superjob_parse.py

--- superjob_parse.py
+++ superjob_parse.py
@@ -68,30 +68,43 @@ def calculate_predicted_salaries(parsed_response):
     return list(filter(None, predicted_salaries))
 
 
+def gather_statistics(api_key, language):
+    """Gathering statistics such as average salary and total vacancies found
+    for a specified language.
+
+    :param language: language to search vacancies on
+    :return: gathered statistics
+    """
+
+    predicted_salaries = list()
+    look_another_page = True
+    page_number = 0
+
+    while look_another_page:
+        response = get_response(superjob_api_key, language, page_number)
+        look_another_page = response.get("more")
+        predicted_salaries += calculate_predicted_salaries(response)
+        total = response.get("total")
+        page_number += 1
+
+    return format_statistics(total, predicted_salaries)
+
+
 if __name__ == "__main__":
     load_dotenv()
     superjob_api_key = os.environ["SUPERJOB_API_TOKEN"]
 
     statistics = dict()
     languages = extract_popular_programming_languages()
-    for language in languages[1:]:
-        predicted_salaries = list()
-        look_another_page = True
-        page_number = 0
-
-        while look_another_page:
-            try:
-                response = get_response(superjob_api_key, language, page_number)
-            except requests.HTTPError:
-                print("SuperJob API is unavailable. Try later.")
-                break
-
-            look_another_page = response.get("more")
-            predicted_salaries += calculate_predicted_salaries(response)
-            total = response.get("total")
-            page_number += 1
-
-        statistics[language] = format_statistics(total, predicted_salaries)
+    for language in languages:
+        try:
+            statistics[language] = gather_statistics(
+                superjob_api_key,
+                language,
+            )
+        except requests.HTTPError:
+            print("SuperJob API is unavailable. Try later.")
+            break
 
     table = generate_pretty_statistics(statistics, "SuperJob")
     print(table)
